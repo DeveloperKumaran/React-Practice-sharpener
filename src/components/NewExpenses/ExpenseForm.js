0ba48866e18affe1d29882713796589813297011
kumaran s <kumarandeveloper77@gmail.com>
2023-11-14 23:26:56 +0530

    form functionality added

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,33 +2,60 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
+
+
+
 const ExpenseForm = () => {
 
-
-    const [enteredTitle,setEnteredTitle] = useState('');
-    const [enteredAmount,setEnteredAmount] = useState('');
-    const [enteredDate,setEnteredDate] = useState('');
+   
+        const [enteredTitle, setEnteredTitle] = useState('');
+        const [enteredAmount, setEnteredAmount] = useState('');
+        const [enteredDate, setEnteredDate] = useState('');
+      
+   // const[userInput,setUserInput] = useState({
+      // enteredTitle:'',
+      // enteredAmount:'',
+       //enteredDate:'' 
+    //})
 
     const titleChangeHandler = (event)=>{
         
-        setEnteredTitle(event.target.value);
-        console.log(event.target.value);
-        console.log(event)
+       //setUserInput({
+       // ...userInput,
+        //enteredTitle: event.target.value
+       //})
         
     }
    
     const amountHandler = (event)=>{
-        setEnteredAmount(event.target.value);
+        //setUserInput({
+           // ...userInput,
+           // enteredAmount:(event.target.value)
+        //});
     }
 
     const dateHandler = (event)=>{
-        setEnteredDate(event.target.value);
+        //setUserInput({
+            //...userInput,
+           // enteredDate:event.target.value
+       // });
     }
+   
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
 
+        console.log(expenseData);
+    }
+    
 
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>

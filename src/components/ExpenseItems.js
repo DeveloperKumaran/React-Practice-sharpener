2c0f7fb6c48417472e00a43cc7de44d31bc501e5
kumaran s <kumarandeveloper77@gmail.com>
2023-11-14 11:21:47 +0530

    expense addes

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,17 @@
+
+
+
+function ExpenseItem() {
+    return (
+      <div>
+        <h1>Expense Items</h1>
+        <ul>
+          <li>Food 100rs</li>
+          <li>Clothing 200rs</li>
+          <li>Travelling 1000rs</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;

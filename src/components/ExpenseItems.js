cd0b52fb278ae2efa6579ec12291e4224149a058
kumaran s <kumarandeveloper77@gmail.com>
2023-11-14 12:21:34 +0530

    props added

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -3,13 +3,13 @@ import './ExpenseItem.css';
 
 const locationOfExpenditure = 'london'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     return (
       <div className="expense-item">
         <div>Expense Items</div>
         <div className="expense-item__description">
-              <h2>car insurance</h2>
-              <div className="expense-item__price">$400.00</div>
+              <h2>{props.title}</h2>
+              <div className="expense-item__price">{props.amount}</div>
         </div>
 
         <h1>{locationOfExpenditure}</h1>

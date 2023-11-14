95452c57b0562ff1ee4308275fe39fb877a8cceb
kumaran s <kumarandeveloper77@gmail.com>
2023-11-15 01:41:11 +0530

    filter added

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,4 +1,33 @@
+
 import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
+
+
+
+
+
+
+/*import React from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
@@ -11,4 +40,4 @@ const NewExpense = () => {
   );
 };
 
-export default NewExpense;
+export default NewExpense;*/
